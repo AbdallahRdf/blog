a02a756bde44323088e48e198445b769c166031c
AbdallahRdf <abdallahradfi@gmail.com>
2024-10-04 23:02:17 +0100

    added styles to the login page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
 function Layout() {
+    
+    const { pathname } = useLocation();
+
     return (
+        (pathname !== "/login" && pathname !== "/login")
+        ?
         <div className='flex flex-col min-h-screen w-full'>
             <div className='w-full xl:w-[1280px] mx-auto relative flex-1'>
                 <Navbar />
@@ -14,6 +19,8 @@ function Layout() {
             </div>
             <Footer />
         </div>
+        :
+        <Outlet />
     )
 }
 
